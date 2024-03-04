559bda60fe141cabe9f7ceb32cfff9ac2802d94d
Fergie77 <48885644+Fergie77@users.noreply.github.com>
2024-03-04 11:21:24 +0000

    Update Animations.js

--- src/features/Animations.js
+++ src/features/Animations.js
@@ -162,7 +162,12 @@ export const floatingBottle = () => {
   }
 
   gsap.registerPlugin(Flip)
-  ScrollTrigger.normalizeScroll(true)
+  ScrollTrigger.normalizeScroll({
+    allowNestedScroll: true,
+    lockAxis: false,
+    momentum: (self) => Math.min(3, self.velocityY / 1000), // dynamically control the duration of the momentum when flick-scrolling
+    type: 'touch,wheel,pointer', // now the page will be drag-scrollable on desktop because "pointer" is in the list
+  })
 
   function createTimeline(componentEl, componentIndex) {
     let originEl = componentEl.querySelectorAll(
