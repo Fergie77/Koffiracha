0bdf38e3af3721ad9b0f215aef9ab9db760db0dd
Fergie77 <48885644+Fergie77@users.noreply.github.com>
2024-05-01 13:45:11 +0100

    u

--- src/features/Animations.js
+++ src/features/Animations.js
@@ -364,8 +364,6 @@ export const pourLottieAnimations = (pageContainer) => {
     let containers = pageContainer.querySelectorAll(
       '.pour_background-animation'
     )
-    // ScrollTrigger.sort()
-    // ScrollTrigger.refresh()
     if (containers) {
       containers.forEach((container) => {
         const lottieColour = container.getAttribute('lottie-colour')
--- src/features/RotatingBottle.js
+++ src/features/RotatingBottle.js
@@ -9,48 +9,49 @@ gsap.registerPlugin(Flip)
 export const rotatingBottleAnimation = (pageContainer) => {
   var container = pageContainer.querySelectorAll('[lottie-element="mobile"]')
 
-  container.forEach((element) => {
-    const triggerElement = element.closest('.floating-bottle-section_wrapper')
-    const endTrigger = triggerElement.querySelector('#endTrigger')
-    console.log(triggerElement, endTrigger)
-    let animationID =
-      element.getAttribute('title')?.toString() || 'chilli-flame' // Default to 'V2' if no title
+  setTimeout(() => {
+    container.forEach((element) => {
+      const triggerElement = element.closest('.floating-bottle-section_wrapper')
+      const endTrigger = triggerElement.querySelector('#endTrigger')
+      let animationID =
+        element.getAttribute('title')?.toString() || 'chilli-flame' // Default to 'V2' if no title
 
-    var jsonPath = `https://cdn.shopify.com/s/files/1/0551/2983/3536/files/${
-      animationID + '-mobile'
-    }.json`
-    var animation = Lottie.loadAnimation({
-      container: element,
-      renderer: 'canvas',
-      loop: false,
-      autoplay: false,
-      path: jsonPath,
-    })
-    const origin = triggerElement.querySelector('.bottle-origin2')
-    const state = Flip.getState(element)
-    origin.appendChild(element)
-    // FLIP!
-    const flip = Flip.to(state, {
-      duration: 5,
-      absolute: true,
-      ease: 'power1.inOut',
-    })
+      var jsonPath = `https://cdn.shopify.com/s/files/1/0551/2983/3536/files/${
+        animationID + '-mobile'
+      }.json`
+      var animation = Lottie.loadAnimation({
+        container: element,
+        renderer: 'canvas',
+        loop: false,
+        autoplay: false,
+        path: jsonPath,
+      })
+      const origin = triggerElement.querySelector('.bottle-origin2')
+      const state = Flip.getState(element)
+      origin.appendChild(element)
+      // FLIP!
+      const flip = Flip.to(state, {
+        duration: 5,
+        absolute: true,
+        ease: 'power1.inOut',
+      })
 
-    const frameCount = 50
-    ScrollTrigger.create({
-      trigger: triggerElement,
-      start: 'top 20%',
-      end: 'bottom bottom',
-      endTrigger: endTrigger,
-      markers: true,
-      animation: flip,
-      scrub: true,
-      onUpdate: (self) => {
-        let frame = Math.floor(self.progress * frameCount)
-        animation.goToAndStop(frame, true)
-      },
+      const frameCount = 50
+      ScrollTrigger.create({
+        trigger: triggerElement,
+        start: 'top 20%',
+        end: 'bottom bottom',
+        endTrigger: endTrigger,
+        markers: true,
+        animation: flip,
+        scrub: true,
+        onUpdate: (self) => {
+          let frame = Math.floor(self.progress * frameCount)
+          animation.goToAndStop(frame, true)
+        },
+      })
     })
-  })
+  }, 1000)
 
   ScrollTrigger.sort()
   ScrollTrigger.refresh()
